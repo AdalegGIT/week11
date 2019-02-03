4e8d5b5c594d65ed401289706538a6f985f0bcda
harisree06 <harisree.n.s@gmail.com>
2019-02-03 09:55:38 -0500

    pushing in class code

---
+++ test/server.rb
@@ -0,0 +1,40 @@
+require 'sinatra'
+
+require_relative 'recipe'
+
+get '/' do
+  redirect to('/recipes')
+end
+
+get '/about' do
+  @title = 'About Us'
+  erb :about
+end
+
+get '/contact' do
+  @title = 'Contact Us'
+  erb :contact
+end
+
+get '/legal' do
+  @title = 'Legal Stuff'
+  erb :legal
+end
+
+get '/recipes' do
+  @title = 'Recipes'
+
+  @recipes = Recipe.all
+
+  erb :recipes
+end
+
+get '/recipes/:id' do
+
+  id = params[:id]
+  @recipe = Recipe.find(id)
+  @title = @recipe.name
+
+  erb :recipe
+
+end
